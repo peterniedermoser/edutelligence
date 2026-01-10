330f2dd11a56ff948967de02024e250ee60ac48b
Peter Niedermoser <peter.niedermoser@live.at>
2026-01-11 00:01:09 +0100

    made monkeypatch work (patched connect_to_weaviate instead of whole __Init__)

--- iris/tests/conftest.py
+++ iris/tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 from pathlib import Path
 
 import pytest
@@ -10,19 +11,29 @@ from testcontainers.core.container import DockerContainer
 
 ROOT = Path(__file__).resolve().parents[1]
 
-os.environ["APPLICATION_YML_PATH"] = str(ROOT / "application.local.yml")
-os.environ["LLM_CONFIG_PATH"] = str(ROOT / "llm_config.local.yml")
+APPLICATION_YML_PATH = str(ROOT / "application.local.yml")
+LLM_CONFIG_PATH = str(ROOT / "llm_config.local.yml")
 
-TEST_WEAVIATE_HOST = "localhost"
-TEST_WEAVIATE_PORT = 8002
-TEST_WEAVIATE_GRPC_PORT = 50052
+os.environ["APPLICATION_YML_PATH"] = APPLICATION_YML_PATH
+os.environ["LLM_CONFIG_PATH"] = LLM_CONFIG_PATH
+
+with open(APPLICATION_YML_PATH, "r") as f:
+    config = yaml.safe_load(f)
+WEAVIATE_HOST = config["test"]["weaviate"]["host"]
+
 
 
 @pytest.fixture(scope="session")
 def weaviate_container():
     container = (
         DockerContainer("cr.weaviate.io/semitechnologies/weaviate:1.32.2")
-        .with_exposed_ports(TEST_WEAVIATE_PORT, TEST_WEAVIATE_GRPC_PORT)
+        .with_name("weaviate_test")
+        .with_command([
+            "--host", "0.0.0.0",
+            "--port", "8001",
+            "--scheme", "http"
+        ])
+        .with_exposed_ports(8001, 50051)
         .with_env("QUERY_DEFAULTS_LIMIT", "25")
         .with_env("AUTHENTICATION_ANONYMOUS_ACCESS_ENABLED", "true")
         .with_env("DEFAULT_VECTORIZER_MODULE", "none")
@@ -33,14 +44,15 @@ def weaviate_container():
 
     yield container
 
-    # container.stop()
+    container.stop()
+
 
 
 import time
 import requests
 
 def wait_for_weaviate(port, timeout=30):
-    url = f"http://localhost:{port}/v1/.well-known/ready"
+    url = f"http://{WEAVIATE_HOST}:{port}/v1/.well-known/ready"
     start = time.time()
     while time.time() - start < timeout:
         try:
@@ -48,47 +60,31 @@ def wait_for_weaviate(port, timeout=30):
                 return True
         except requests.exceptions.ConnectionError:
             time.sleep(0.5)
-    return True
+    return RuntimeError("Weaviate did not become ready in time")
+
 
 
 @pytest.fixture(scope="session")
 def weaviate_client(weaviate_container):
 
-    if wait_for_weaviate(int(weaviate_container.get_exposed_port(TEST_WEAVIATE_PORT))):
+    if wait_for_weaviate(int(weaviate_container.get_exposed_port(8001))):
         client = weaviate.connect_to_local(
-        host=TEST_WEAVIATE_HOST,
-        port=int(weaviate_container.get_exposed_port(TEST_WEAVIATE_PORT)),
-        grpc_port=int(weaviate_container.get_exposed_port(TEST_WEAVIATE_GRPC_PORT)))
+        host=WEAVIATE_HOST,
+        port=int(weaviate_container.get_exposed_port(8001)),
+        grpc_port=int(weaviate_container.get_exposed_port(50051)))
 
 
         yield client
 
-    # client.close()
+        client.close()
 
 
-from iris.vector_database.database import VectorDatabase
-from iris.vector_database.lecture_transcription_schema import init_lecture_transcription_schema
-from iris.vector_database.lecture_unit_page_chunk_schema import init_lecture_unit_page_chunk_schema
-from iris.vector_database.lecture_unit_schema import init_lecture_unit_schema
-from iris.vector_database.lecture_unit_segment_schema import init_lecture_unit_segment_schema
-from iris.vector_database.faq_schema import init_faq_schema
 
+# Connect with Test DB instead of real one
 @pytest.fixture(autouse=True)
-def patch_vector_db_init(weaviate_client):
-
-    def _mocked_init(self):
-
-        print("my init is in use")
-
-        # Always use Test Client
-        VectorDatabase._client_instance = weaviate_client
-
-        self.client = VectorDatabase._client_instance
-        self.lectures = init_lecture_unit_page_chunk_schema(self.client)
-        self.transcriptions = init_lecture_transcription_schema(self.client)
-        self.lecture_segments = init_lecture_unit_segment_schema(self.client)
-        self.lecture_units = init_lecture_unit_schema(self.client)
-        self.faqs = init_faq_schema(self.client)
-
-    with patch.object(VectorDatabase, "__init__", _mocked_init):
+def patch_weaviate_connect(weaviate_client):
+    with patch(
+            "iris.vector_database.database.weaviate.connect_to_local",
+            return_value=weaviate_client,
+    ):
         yield
